be70d17741797cca957b6a906010735d970be443
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-13 22:32:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function validEmail(str) {
   //your JS code here.
+	//abho@g.com
+	if(str[0]=="@" || str.slice(-1)=="@") return false;
+	if(str[0] =="." || str.slice(-1) ==".") return false;
+	
+	if(str.includes("@") && str.includes(".")){
+		return true;
+		
+	}else return false;
+	
 }
 
-// Do not change the code below.
+// Do not change the co.de bfelow.vf
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
